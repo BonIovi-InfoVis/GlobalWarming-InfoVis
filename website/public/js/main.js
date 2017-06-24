9bd8d62e5a5fad9b1ee279baa822df4aeeb299a5
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-06-24 19:08:13 +0200

    done main page and renamed satellite pages

--- website/public/js/main.js
+++ website/public/js/main.js
@@ -1,1 +1,27 @@
+var pages = ['landing.html', 'js/globeEmissions/index.html', 'js/globeTemp/index.html', 'js/iceMassRecession/index.html', 'js/seaLevel/index.html', 'js/tempRise/index.html'];
+var current = 0;
+var descriptions = ['', 'prova'];
+var titles = ['', 'prova'];
 
+
+var increment = function() {
+    current++;
+}
+var loadIframe = function() {
+    $('.iframe').attr('src', pages[current % pages.length]);
+}
+var changeText = function() {
+    $('.title').text(titles[current % titles.length]);
+    $('.description').text(descriptions[current % descriptions.length]);
+}
+
+$(document).ready(function() {
+    loadIframe();
+    changeText();
+    $(".next").click(function() {
+
+        increment();
+        loadIframe();
+        changeText();
+    });
+});
